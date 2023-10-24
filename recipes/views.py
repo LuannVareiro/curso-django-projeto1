78ad1ff33d2deb0ce2663e4662c47a493e758c8d
LuannVareiro <luann.hjob@gmail.com>
2023-10-24 18:48:29 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,11 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Luann Henrique'
     }, status=201)
-
-
-def contato(request):
-    return HttpResponse('CONTATO 1')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE 2')
